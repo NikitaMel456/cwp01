c1e065a8eebeb27850519a55f4ed7ba887b55103
Nikita <nikita.melentev13@hotmail.com>
2017-09-25 22:20:27 +0300

    Almost ready poj

--- node.js
+++ node.js
@@ -8,47 +8,97 @@ let pt = process.argv[2];
 
 const name='\\summary.js';
 let pathh=pt+name;
-var getfiles="const fs=require('fs');\n" +
-    "const path=require('path');\n \
-    const pathh='"+pt+"';\
-    const getFiles = function (dir, files_){\n \
-        files_ = files_ || [];\n \
-        var files = fs.readdirSync(dir);\n \
-        for (var i in files){\n\
-            var name = dir + '/' + files[i];\n \
-            console.log(dir + '/' + files[i]);\n \
-            if (fs.statSync(name).isDirectory()){\n \
-                getFiles(name, files_);\n \
-            } else {\n\
-                files_.push(name);\n\
-            }\n\
-        }\n\
-        return files_;\n\
-    };\n\
-    getFiles('"+pt+"');\n " +
-    "" +
-    "function createDir(pathh){\n" +
-    "let dirs = path.formate().split('\\');\n" +
-    "        let prevDir = dirs.pop();\n" +
-    "        if(prevDir == '')\n" +
-    "{\n" +
-    "            prevDir = dirs.pop();\n" +
+const getfiles="try {\n" +
+    "    const fs = require('fs');\n" +
+    "    const path = require('path');\n" +
+    "    const pathh = '"+pt+"';\n" +
+    "    const getFiles = function (dir, files_) {\n" +
+    "        files_ = files_ || [];\n" +
+    "        let files = fs.readdirSync(dir);\n" +
+    "        for (var i in files) {\n" +
+    "            var name = dir + '/' + files[i];\n" +
+    "            console.log(dir + '/' + files[i]);\n" +
+    "            if (fs.statSync(name).isDirectory()) {\n" +
+    "                getFiles(name, files_);\n" +
+    "            } else {\n" +
+    "                files_.push(name);\n" +
+    "            }\n" +
     "        }\n" +
-    " let new_directory = pathh + '/' + prevDir;\n" +
-    "        fs.mkdir(new_directory, function (err) {\n" +
-    "        if (err)\n" +
-    "{\n" +
-    "            console.error('Произошла ошибка при создании директории');\n" +
+    "        return files_;\n" +
+    "    };\n" +
+    "    getFiles('"+pt+"');\n" +
+    "\n" +
+    "\n" +
+    "    let createdDir=null;\n" +
+    "\n" +
+    "    function createDir(pathh) {\n" +
+    "        let direction = pathh.split('\\\\');\n" +
+    "        let prevDir = direction.pop();\n" +
+    "        if (prevDir == '') {\n" +
+    "            prevDir = direction.pop();\n" +
     "        }\n" +
-    "else{console.log('Директория создана успешно');}" +
-    "    });\n" +
-    "    return new_directory;};\n" +
-    "createDir(pathh);\n" ;
+    "        let newDir = pathh + '/' + prevDir;\n" +
+    "        fs.mkdir(newDir, function (err) {\n" +
+    "            if (err) {\n" +
+    "                console.error('Произошла ошибка при создании директории либо дериктория  с таким именем уже существует');\n" +
+    "            }\n" +
+    "            else {\n" +
+    "                console.error('Директория создана успешно');\n" +
+    "            }\n" +
+    "        });\n" +
+    "        createdDir = newDir;\n" +
+    "    };\n" +
+    "    createDir(pathh);\n" +
+    "\n" +
+    "\n" +
+    "    function copyFile(firstDir, newDir) {\n" +
+    "        fs.readFile('C:\\Users\\Никита\\cwp01\\config.json','utf8', function (err, str) {\n" +
+    "            if (err) {\n" +
+    "                console.error('Произошла ошибка чтения файла json');\n" +
+    "            }\n" +
+    "            else {\n" +
+    "                let jsonstrr=JSON.stringify(JSON.parse(str));;\n" +
+    "                let jsonstr=jsonstrr.replace(/{\"copyright\":|} /i,' ');\n" +
+    "                readDir(firstDir,newDir,jsonstr)\n" +
+    "            }\n" +
+    "        });\n" +
+    "    }\n" +
+    "    function readDir(firstDir, newDir, jsonstr) {\n" +
+    "        fs.readdir(firstDir, function (error, files) {\n" +
+    "            files.forEach(function (item) {\n" +
+    "                fs.stat(firstDir + '/' + item, function (error, dir) {\n" +
+    "                    if (!dir.isDirectory()) {\n" +
+    "                        if (path.extname(item).toLowerCase() == '.txt')\n" +
+    "                        {writeIntoFile(firstDir, newDir, jsonstr, item);}\n" +
+    "                    }\n" +
+    "                });\n" +
+    "            });\n" +
+    "        });\n" +
+    "    }\n" +
+    "\n" +
+    "    function writeIntoFile(firstDir, newDir, jsonstr, item) {\n" +
+    "        fs.readFile(firstDir + '/' + item, function (err, data) {\n" +
+    "            if (err) {\n" +
+    "                console.error('Произошла ошибка чтения файла');\n" +
+    "            }\n" +
+    "            else {\n" +
+    "                console.error('Чтение файла завершено');\n" +
+    "          let newFile = jsonstr + data.toString() + jsonstr;\n" +
+    "                fs.writeFile(newDir + '/' + item, newFile, 'utf8', function (err) {\n" +
+    "                    if (err) console.error(\"Произошла ошибка при записи\");\n" +
+    "                });\n" +
+    "            }\n" +
+    "        });\n" +
+    "    }\n" +
+    "\n" +
+    "   copyFile(pathh,createdDir);\n" +
+    "}\n" +
+    "catch(exception){exception.message.toString();}";
 
 // noinspection JSAnnotator
 fs.open(pathh, "w+", 0644, function(err, file_handle) {
     if (!err) {
-        fs.write(file_handle,getfiles, null, 'UTF-8', function(err, written) {
+        fs.write(file_handle,getfiles, null, 'UTF-8', function(err) {
             if (!err) {
                 console.log("Текст успешно записан в файл");
 
@@ -60,5 +110,4 @@ fs.open(pathh, "w+", 0644, function(err, file_handle) {
         console.log("Произошла ошибка при открытии");
     }
 });
-
 
